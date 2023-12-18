40079b7fafd44e8cb6ba0ae44835195c8f87be91
AistVolhv <istanik@ya.ru>
2023-12-18 10:15:35 +0500

    Исправление первой функции. Создание второй.

--- js/functions.js
+++ js/functions.js
@@ -4,18 +4,31 @@ function checkPalindrome (palindrome) {
   let strokaWithoutSpace = '';
   let palindromeLow = palindrome.toLowerCase();
 
-
   if (palindromeLow.indexOf(' ') === -1) {
     strokaContrary = palindromeLow.slice(-palindromeLow.length);
     if (strokaContrary === palindromeLow) console.log('Строка ' + palindrome + ' - является полиндромом!')
     else console.log('Строка ' + palindrome + ' - НЕ является полиндромом!');
   }
   else {
-    strokaWithoutSpace = replaceAll(palindromeLow,' ','');
-    for (let i = strokaWithoutSpace.length - 1; i >= 0; i--) {
-      strokaContrary = strokaContrary + strokaWithoutSpace[i];
-    }
-    if (strokaContrary === strokaWithoutSpace) console.log('Строка ' + palindrome + ' - является полиндромом!')
-    else console.log('Строка ' + palindrome + ' - НЕ является полиндромом!');
+    strokaWithoutSpace = palindromeLow.replaceAll(' ','');
+    console.log('strokaWithoutSpace= ' + strokaWithoutSpace);
+    strokaContrary = strokaWithoutSpace.slice(-strokaWithoutSpace.length);
+
+    if (strokaContrary === strokaWithoutSpace) console.log('Строка "' + palindrome + '" - является полиндромом!')
+    else console.log('Строка "' + palindrome + '" - НЕ является полиндромом!');
   }
+}
+
+function extractNumber(strokaWithNumber){
+  let numberFromStroka = '';
+  console.log(strokaWithNumber);
+  for (let i = 0; i <= strokaWithNumber.length; i++){
+    console.log(strokaWithNumber[i]);
+
+    if (strokaWithNumber[i].typeof === 'number') {
+      numberFromStroka += strokaWithNumber[i];
+    }
+  }
+  console.log('Строка изначальная - ' + strokaWithNumber);
+  console.log('Число из строки - ' + numberFromStroka);
 }
