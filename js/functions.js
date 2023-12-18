28be6ecaa750e8e0f5c999ea91ac88fa6ee77a27
AistVolhv <istanik@ya.ru>
2023-12-18 13:14:35 +0500

    Добавление третей функции

--- js/functions.js
+++ js/functions.js
@@ -29,3 +29,36 @@ function extractNumber(strokaWithNumber){
   console.log('Строка изначальная - ' + strokaWithNumber);
   console.log('Число из строки - ' + numberFromStroka);
 }
+
+function addStrokaToStroka (beginStroka, minLengthS, addStroka){
+  //
+  let beetwenStroka = '';
+  let resultStroka = '';
+  let difference = 0;
+  let nomer = 0;
+  //
+  if (beginStroka.length <= minLengthS) {
+    if (beginStroka.length + addStroka.length > minLengthS) {
+      console.log('Путь 1');
+      difference = minLengthS - beginStroka.length;
+      console.log('difference =' + difference);
+      resultStroka = addStroka.slice(0,difference) + beginStroka;
+    }
+    else {
+      console.log('Путь addStroka + beginStroka');
+      beetwenStroka = addStroka + beginStroka;
+      nomer = 0;
+      while (beetwenStroka.length < minLengthS) {
+        beetwenStroka += addStroka[nomer];
+        nomer += 1;
+      }
+      resultStroka = beetwenStroka;
+    }
+
+    //for (let i = 0; i < minLengthS; i++)
+    //      resultStroka += beetwenStroka[i];
+    //
+  }
+  console.log('Вызываем функцию: addStrokaToStroka ("' + beginStroka + '", ' + minLengthS + ', "' + addStroka + '")');
+  console.log(resultStroka);
+}
