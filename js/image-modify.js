e4de59fb36eb1d0bf4881489d22349496f141d89
AistVolhv <istanik@ya.ru>
2024-02-27 15:40:55 +0500

    Исправление ошибки в index.html; Проба с фильтрами...

--- js/image-modify.js
+++ js/image-modify.js
@@ -4,6 +4,24 @@ const buttonScaleControlBigger = document.querySelector('.scale__control--bigger
 
 const imageUploadPreview = document.querySelector('.img-upload__preview');
 
+const listEffects = document.querySelector('.effects__list');
+
+
+
+function onClickListEffects (evt) {
+  const liElements = listEffects.children;
+  console.log('liElements = ' + liElements);
+  for (let liItem of liElements) {
+    console.log('liItem = ' + liItem);
+    const radioButton = liItem.querySelector('input[type="radio"]');
+    console.log('radioButton = ' + radioButton);
+    console.log('radioButton.id = ' + radioButton.id);
+    if (evt.target.id === radioButton.id) {
+      console.log('Урррра мы наши элемент - это ' + radioButton.id);
+    }
+  }
+}
+
 function onClickButtonScaleControlSmaller () {
   if (buttonScaleControlValue.value > 25 && buttonScaleControlValue.value <= 100) {
     const rangeValue = parseInt(buttonScaleControlValue.value, 10);
@@ -29,12 +47,14 @@ function addEventOnElementsWrapper () {
   buttonScaleControlSmaller.addEventListener('click',onClickButtonScaleControlSmaller);
   buttonScaleControlBigger.addEventListener('click',onClickButtonScaleControlBigger);
   buttonScaleControlValue.addEventListener('change',onChangeScaleControlValue);
+  listEffects.addEventListener('click', onClickListEffects);
 }
 
 function removeEventOnElementsWrapper () {
   buttonScaleControlSmaller.removeEventListener('click',onClickButtonScaleControlSmaller);
   buttonScaleControlBigger.removeEventListener('click',onClickButtonScaleControlBigger);
   buttonScaleControlValue.removeEventListener('change',onChangeScaleControlValue);
+  listEffects.removeEventListener('click', onClickListEffects);
 }
 
 export {addEventOnElementsWrapper, removeEventOnElementsWrapper};
