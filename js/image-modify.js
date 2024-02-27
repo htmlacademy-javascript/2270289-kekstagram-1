829f787c1fd26b71175c345dfe8e8df35ea82609
AistVolhv <istanik@ya.ru>
2024-02-27 14:19:55 +0500

    Решили наконец-то вопрос масштабирования...

--- js/image-modify.js
+++ js/image-modify.js
@@ -4,20 +4,63 @@ const buttonScaleControlBigger = document.querySelector('.scale__control--bigger
 
 const imageUploadPreview = document.querySelector('.img-upload__preview');
 
+const imgUploadPreviewContainer = document.querySelector('.img-upload__preview-container');
+
+function onClickImgUploadPreviewContainer (evt) {
+  /*
+  console.log('Клик на контейнере');
+  const rangeValue = buttonScaleControlValue.value;
+  console.log('rangeValue = ' + rangeValue);
+  if (evt.target.id === 'button-smaller-image') {
+    //
+    //buttonScaleControlValue.value = rangeValue > 25 && rangeValue <= 100 ? rangeValue - 25 : rangeValue;
+    if (buttonScaleControlValue.value > 25 && buttonScaleControlValue.value <= 100) {
+      buttonScaleControlValue.value -= 25;
+      console.log('buttonScaleControlValue.value (СТАЛО) = ' + buttonScaleControlValue.value);
+    }
+    onChangeScaleControlValue();
+    console.log('buttonScaleControlValue.value = ' + buttonScaleControlValue.value);
+    return;
+  }
+  if (evt.target.id === 'button-bigger-image') {
+    //
+    //buttonScaleControlValue.value = rangeValue >= 25 && rangeValue < 100 ? rangeValue + 25 : rangeValue;
+    if (buttonScaleControlValue.value < 100 && buttonScaleControlValue.value >= 25) {
+      buttonScaleControlValue.value += 25;
+      console.log('buttonScaleControlValue.value (БЫЛО) = ' + buttonScaleControlValue.value);
+    }
+    onChangeScaleControlValue();
+    console.log('buttonScaleControlValue.value = ' + buttonScaleControlValue.value);
+    return;
+  }
+  */
+}
+
 function onClickButtonScaleControlSmaller () {
-  if (buttonScaleControlValue.value > 25 && buttonScaleControlValue.value < 100) {
-    buttonScaleControlValue.value -= 25;
+  //console.log('Клик на кнопкке Меньше внешне');
+  //console.log('buttonScaleControlValue.value (БЫЛО) = ' + buttonScaleControlValue.value);
+  if (buttonScaleControlValue.value > 25 && buttonScaleControlValue.value <= 100) {
+    const rangeValue = parseInt(buttonScaleControlValue.value, 10);
+    buttonScaleControlValue.value = rangeValue - 25;
+    //buttonScaleControlValue.value -= 25;
+    onChangeScaleControlValue();
+    //console.log('buttonScaleControlValue.value (СТАЛО) = ' + buttonScaleControlValue.value);
   }
 }
 
 function onClickButtonScaleControlBigger () {
-  if (buttonScaleControlValue.value < 100 && buttonScaleControlValue.value > 25) {
-    buttonScaleControlValue.value += 25;
+  //console.log('Клик на кнопкке Больше внешне');
+  //console.log('buttonScaleControlValue.value (БЫЛО) = ' + buttonScaleControlValue.value);
+  if (buttonScaleControlValue.value < 100 && buttonScaleControlValue.value >= 25) {
+    const rangeValue = parseInt(buttonScaleControlValue.value, 10);
+    buttonScaleControlValue.value = rangeValue + 25;
+    onChangeScaleControlValue();
+    //console.log('buttonScaleControlValue.value (БЫЛО) = ' + buttonScaleControlValue.value);
   }
 }
 
 function onChangeScaleControlValue () {
-  const scale = buttonScaleControlValue.value / 100;
+  const scale = parseInt(buttonScaleControlValue.value, 10) / 100;
   imageUploadPreview.attributeStyleMap.set('transform', `scale(${scale})`);
 }
 
@@ -25,12 +68,14 @@ function addEventOnElementsWrapper () {
   buttonScaleControlSmaller.addEventListener('click',onClickButtonScaleControlSmaller);
   buttonScaleControlBigger.addEventListener('click',onClickButtonScaleControlBigger);
   buttonScaleControlValue.addEventListener('change',onChangeScaleControlValue);
+  imgUploadPreviewContainer.addEventListener('click',onClickImgUploadPreviewContainer);
 }
 
 function removeEventOnElementsWrapper () {
   buttonScaleControlSmaller.removeEventListener('click',onClickButtonScaleControlSmaller);
   buttonScaleControlBigger.removeEventListener('click',onClickButtonScaleControlBigger);
   buttonScaleControlValue.removeEventListener('change',onChangeScaleControlValue);
+  imgUploadPreviewContainer.removeEventListener('click',onClickImgUploadPreviewContainer);
 }
 
 export {addEventOnElementsWrapper, removeEventOnElementsWrapper};
