15d6329df13c6ebeebe6bc3617317819fa76d298
AistVolhv <istanik@ya.ru>
2024-02-28 10:43:25 +0500

    Создание словарей, модификация функций.

--- js/image-modify.js
+++ js/image-modify.js
@@ -2,12 +2,35 @@ const buttonScaleControlValue = document.querySelector('.scale__control--value')
 const buttonScaleControlSmaller = document.querySelector('.scale__control--smaller');
 const buttonScaleControlBigger = document.querySelector('.scale__control--bigger');
 
-const imageUploadPreview = document.querySelector('.img-upload__preview');
+const divImgUploadPreview = document.querySelector('.img-upload__preview');
+const imagePreview = divImgUploadPreview.querySelector('img');
 
 const listEffects = document.querySelector('.effects__list');
 
+const fieldSetForUiSlider = document.querySelector('.img-upload__effect-level');
+const sliderValue = fieldSetForUiSlider.querySelector('.effect-level__value');
+const sliderElement = fieldSetForUiSlider.querySelector('.effect-level__slider');
+
+
+noUiSlider.create(sliderElement,{
+  range : {
+    min: 0,
+    max: 100,
+  },
+  start: 100,
+  step: 1,
+  connect: 'lower',
+});
+
+sliderElement.noUiSlider.on('update', (...rest) => {
+  sliderValue.value = sliderElement.noUiSlider.get();
+  console.log(sliderValue.value);
+  console.log(rest);
+});
+
 //const listIdRadioButtons = ['effect-none','effect-chrome','effect-sepia','effect-marvin','effect-phobos','effect-heat'];
-const effectsMap = {
+
+const effectToIdMap = {
   none : 'effect-none',
   chrome: 'effect-chrome',
   sepia: 'effect-sepia',
@@ -16,32 +39,97 @@ const effectsMap = {
   heat: 'effect-heat'
 };
 
+const inputIdToEffectMap = {
+  [effectToIdMap.none] : 'none',
+  [effectToIdMap.chrome] : 'chrome',
+  [effectToIdMap.sepia] : 'sepia',
+  [effectToIdMap.marvin] : 'marvin',
+  [effectToIdMap.phobos] : 'phobos',
+  [effectToIdMap.heat] : 'heat'
+};
+
+const inputIdToClassMap = {
+  [effectToIdMap.none] : `effects__preview--${inputIdToEffectMap[effectToIdMap.none]}`,
+  [effectToIdMap.chrome] : `effects__preview--${inputIdToEffectMap[effectToIdMap.chrome]}`,
+  [effectToIdMap.sepia] : `effects__preview--${inputIdToEffectMap[effectToIdMap.sepia]}`,
+  [effectToIdMap.marvin] : `effects__preview--${inputIdToEffectMap[effectToIdMap.marvin]}`,
+  [effectToIdMap.phobos] : `effects__preview--${inputIdToEffectMap[effectToIdMap.phobos]}`,
+  [effectToIdMap.heat] : `effects__preview--${inputIdToEffectMap[effectToIdMap.heat]}`
+};
+
 function onClickListEffects (evt) {
   // При переключении эффектов, уровень насыщенности сбрасывается до начального значения (100%)
   // : слайдер, CSS-стиль изображения и значение поля должны обновляться.
+  console.log(effectToIdMap);
+  console.log(inputIdToEffectMap);
+  console.log(inputIdToClassMap);
+  console.log(inputIdToClassMap[evt.target.id]);
+  imagePreview.classList.add(inputIdToClassMap[evt.target.id]);
+
   switch (evt.target.id) {
-    case effectsMap.none : {
+    case effectToIdMap.none : {
       // CSS-стили filter удаляются.;  слайдер и его контейнер (элемент .img-upload__effect-level) скрываются.
+
     }
       break;
-    case effectsMap.chrome : {
+    case effectToIdMap.chrome : {
       // filter: grayscale(0..1) с шагом 0.1;
+      sliderElement.noUiSlider.updateOptions({
+        range : {
+          min : 0,
+          max : 1,
+        },
+        start : 1,
+        step : 0.1,
+      });
     }
       break;
-    case effectsMap.sepia : {
+    case effectToIdMap.sepia : {
       // filter: sepia(0..1) с шагом 0.1;
+      sliderElement.noUiSlider.updateOptions({
+        range : {
+          min : 0,
+          max : 1,
+        },
+        start : 1,
+        step : 0.1,
+      });
     }
       break;
-    case effectsMap.marvin : {
+    case effectToIdMap.marvin : {
       // filter: invert(0..100%) с шагом 1%;
+      sliderElement.noUiSlider.updateOptions({
+        range : {
+          min : 0,
+          max : 100,
+        },
+        start : 100,
+        step : 1,
+      });
     }
       break;
-    case effectsMap.phobos : {
+    case effectToIdMap.phobos : {
       // filter: blur(0..3px) с шагом 0.1px;
+      sliderElement.noUiSlider.updateOptions({
+        range : {
+          min : 0,
+          max : 3,
+        },
+        start : 3,
+        step : 0.1,
+      });
     }
       break;
-    case effectsMap.heat : {
+    case effectToIdMap.heat : {
       // filter: brightness(1..3) с шагом 0.1;
+      sliderElement.noUiSlider.updateOptions({
+        range : {
+          min : 1,
+          max : 3,
+        },
+        start : 3,
+        step : 0.1,
+      });
     }
       break;
   }
@@ -65,7 +153,7 @@ function onClickButtonScaleControlBigger () {
 
 function onChangeScaleControlValue () {
   const scale = parseInt(buttonScaleControlValue.value, 10) / 100;
-  imageUploadPreview.attributeStyleMap.set('transform', `scale(${scale})`);
+  imagePreview.attributeStyleMap.set('transform', `scale(${scale})`);
 }
 
 function addEventOnElementsWrapper () {
