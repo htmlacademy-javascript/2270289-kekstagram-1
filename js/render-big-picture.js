63fd6f67e9c7b416b1aa229c6687077b9bf50f77
AistVolhv <istanik@ya.ru>
2024-03-22 14:46:02 +0500

    Изменение модуля рендеринга комментариев.

--- js/render-big-picture.js
+++ js/render-big-picture.js
@@ -1,5 +1,4 @@
-import {isEscapeKey,pluralize} from './utils.js';
-import {createRangeElementsForFragment} from './render-comments.js';
+import { isEscapeKey, pluralize } from './utils.js';
 
 const COUNT_VIEW_COMMENTS = 5;
 
@@ -19,66 +18,91 @@ const pictureCommentTemplateClone = pictureCommentTemplate.cloneNode(true);
 
 let commentsUpBoundary = COUNT_VIEW_COMMENTS;
 let commentsDownBoundary = 0;
+const listComments = [];
+
+const onDocumentKeyDown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    onClickButtonForCloseBigPicture();
+  }
+};
+
+const createRangeElementsForFragment = () => {
+  const commentsFragment = document.createDocumentFragment();
+
+  for (let i = commentsDownBoundary; i < commentsUpBoundary; i++) {
+    const comment = pictureCommentTemplateClone.cloneNode(true);
+    const commentImage = comment.querySelector('img');
+    const commentText = comment.querySelector('p');
+
+    commentImage.src = listComments[i].avatar;
+    commentImage.width = 35;
+    commentImage.height = 35;
+    commentImage.alt = listComments[i].name;
+    commentText.textContent = listComments[i].message;
+
+    commentsFragment.appendChild(comment);
+  }
+  return commentsFragment;
+};
+
+const onClickButtonForLoadNextMessage = () => {
+  const commentDeclension = pluralize(listComments.length, ['комментария', 'комментариев', 'комментариев']); // Для склонения слова
+
+  commentsDownBoundary += COUNT_VIEW_COMMENTS;
+  const increaseBoundary = commentsDownBoundary + COUNT_VIEW_COMMENTS;
+  commentsUpBoundary = increaseBoundary < listComments.length ? increaseBoundary : listComments.length;
+
+  bigPictureSocialComments.appendChild(createRangeElementsForFragment());
+  bigPictureSocialCommentCount.innerHTML = `${commentsUpBoundary} из ${bigPictureCommentsCount.outerHTML} ${commentDeclension}`; // Использую innerHTML, вместо textContent, для того чтобы внутри элемента сохранить тэг span, который был в изначальной разметке.
+  if (commentsUpBoundary === listComments.length) {
+    bigPictureSocialCommentsLoader.classList.add('hidden');
+  }
+};
 
 const openBigPicture = (picture) => {
-  const onDocumentKeyDown = (evt) => {
-    if (isEscapeKey(evt)) {
-      evt.preventDefault();
-      onClickButtonForCloseBigPicture();
-    }
-  };
+  const commentDeclension = pluralize(picture.comments.length, ['комментария', 'комментариев', 'комментариев']); // Для склонения слова
 
-  bigPictureSection.classList.remove('hidden'); // показываем секцию большой фотографии
-  document.body.classList.add('modal-open');
+  commentsDownBoundary = 0;
+  commentsUpBoundary = (picture.comments.length <= 5) ? picture.comments.length : COUNT_VIEW_COMMENTS;
+  listComments.length = 0; // очищаем содиржимое массива
+  picture.comments.forEach((comment) => listComments.push(comment));
 
   bigPicture.src = picture.url;
   bigPictureLikesCount.textContent = picture.likes;
   bigPictureCommentsCount.textContent = picture.comments.length;
+  bigPictureSocialCaption.textContent = picture.description;
 
-  bigPictureCancel.addEventListener('click',onClickButtonForCloseBigPicture); // обработчик на закрытие секции большой фотографии, при щелчке на кнопке закрытия
-  document.addEventListener('keydown',onDocumentKeyDown); // обработчик нажатия клавиш на клавиатуре, на document
-  const commentDeclension = pluralize(picture.comments.length,['комментария','комментариев','комментариев']); // Для склонения слова
-
-  const onClickButtonForLoadNextMessage = () => {
-    commentsDownBoundary += COUNT_VIEW_COMMENTS;
-    const increaseBoundary = commentsDownBoundary + COUNT_VIEW_COMMENTS;
-    commentsUpBoundary = increaseBoundary < picture.comments.length ? increaseBoundary : picture.comments.length;
-
-    bigPictureSocialComments.appendChild(createRangeElementsForFragment(pictureCommentTemplateClone,'img','p',commentsDownBoundary,commentsUpBoundary,picture));
-    bigPictureSocialCommentCount.innerHTML = `${commentsUpBoundary} из ${bigPictureCommentsCount.outerHTML} ${commentDeclension}`; // Использую innerHTML, вместо textContent, для того чтобы внутри элемента сохранить тэг span, который был в изначальной разметке.
-    if (commentsUpBoundary === picture.comments.length) {
-      bigPictureSocialCommentsLoader.classList.add('hidden');
-    }
-  };
-
+  bigPictureSection.classList.remove('hidden'); // показываем секцию большой фотографии
+  document.body.classList.add('modal-open');
   if (picture.comments.length <= 5) {
-    commentsUpBoundary = picture.comments.length;
+    bigPictureSocialCommentsLoader.classList.add('hidden');
   }
+
+  bigPictureCancel.addEventListener('click', onClickButtonForCloseBigPicture); // обработчик на закрытие секции большой фотографии, при щелчке на кнопке закрытия
+  document.addEventListener('keydown', onDocumentKeyDown); // обработчик нажатия клавиш на клавиатуре, на document
 
   bigPictureSocialComments.innerHTML = '';
   bigPictureSocialCommentCount.innerHTML = `${commentsUpBoundary} из ${bigPictureCommentsCount.outerHTML} ${commentDeclension}`;
   bigPictureCommentsCount.textContent = picture.comments.length;
 
-  bigPictureSocialComments.appendChild(createRangeElementsForFragment(pictureCommentTemplateClone,'img','p',commentsDownBoundary,commentsUpBoundary,picture));
+  bigPictureSocialComments.appendChild(createRangeElementsForFragment());
+
   bigPictureSocialCommentsLoader.addEventListener('click', onClickButtonForLoadNextMessage);
-
-  bigPictureSocialCaption.textContent = picture.description;
-  if (picture.comments.length <= 5) {
-    bigPictureSocialCommentsLoader.classList.add('hidden');
-  }
-
-  // Функциональное объявление, для поднятия.
-  function onClickButtonForCloseBigPicture () {
-    bigPictureSection.classList.add('hidden');
-    document.body.classList.remove('modal-open');
-    bigPictureSocialCommentCount.classList.remove('hidden');
-    bigPictureSocialCommentsLoader.classList.remove('hidden');
-    document.removeEventListener('keydown',onDocumentKeyDown);
-    bigPictureSocialCommentsLoader.removeEventListener('click',onClickButtonForLoadNextMessage);
-    bigPictureSocialCommentsLoader.classList.remove('hidden');
-    commentsUpBoundary = COUNT_VIEW_COMMENTS;
-    commentsDownBoundary = 0;
-  }
 };
 
-export {openBigPicture};
+// Функциональное объявление, для поднятия.
+function onClickButtonForCloseBigPicture() {
+  bigPictureSection.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  bigPictureSocialCommentCount.classList.remove('hidden');
+  bigPictureSocialCommentsLoader.classList.remove('hidden');
+  document.removeEventListener('keydown', onDocumentKeyDown);
+  bigPictureSocialCommentsLoader.removeEventListener('click', onClickButtonForLoadNextMessage);
+  bigPictureSocialCommentsLoader.classList.remove('hidden');
+  commentsUpBoundary = COUNT_VIEW_COMMENTS;
+  commentsDownBoundary = 0;
+}
+
+
+export { openBigPicture };
